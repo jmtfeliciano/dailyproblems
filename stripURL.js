be02fc760afaeecee7eb9bd6a88023a8e393226d
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-06 13:44:15 -0800

    Implemented query duplicate deleter using Map()

--- stripURL.js
+++ stripURL.js
@@ -9,5 +9,31 @@ stripUrlParams('www.codewars.com', ['b']) // returns 'www.codewars.com'
 */
 
 function stripUrlParams(url, paramsToStrip){
+  var urlCopy = url.slice();
+  var queryStart = url.indexOf('?');
+  if(queryStart === -1){
+    return url;
+  }
+  var homeLink = url.slice(0,queryStart+1);
+  var parameters = url.slice(queryStart+1, url.length);
+  var parameterArray = parameters.split('&');
 
+  var parMap = new Map();
+  
+  for(var i=0; i<parameterArray.length; i++){
+    parameterArray[i] = parameterArray[i].split('=');
+    parMap.set(parameterArray[i][0], parameterArray[i][1]);
+  }
+
+  
+  var currentKey = 0;
+  for (var [key, value] of parMap) {
+    homeLink = homeLink+key+'='+value;
+    currentKey++;
+    if(currentKey < parMap.size){
+      homeLink += '&';
+    }
+  }
+
+  return homeLink;
 }
