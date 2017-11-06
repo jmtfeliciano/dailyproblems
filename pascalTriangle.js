09713b870dde8a5b123b49f03d3a83356fce2899
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-06 12:37:37 -0800

    created new problem file

---
+++ pascalTriangle.js
@@ -0,0 +1,8 @@
+// task from codewar:
+// pascalsTriangle(4) == [1,1,1,1,2,1,1,3,3,1]
+// Write a function that, given a depth (n), 
+// returns a single-dimensional array representing Pascal's Triangle to the n-th level.
+
+function pascalsTriangle(n) {
+  
+}
