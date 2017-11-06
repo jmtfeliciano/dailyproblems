ea617a480516f4153f114341c7abdbfc0c152c73
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-06 15:28:47 -0800

    Implemented function correctly; utilized decodeURIComponent()

--- queryToObject.js
+++ queryToObject.js
@@ -1,7 +1,7 @@
 /*
 function:  convertQueryToMap(query)
 output:  object
-interesting problem?  yes, because needs to decode 'Light%20Blue' 
+interesting problem?  yes, because needs to decode 'Light%20Blue' via decodeURIComponent();
 */
 
 /*
@@ -20,7 +20,35 @@ and expect convertQueryToMap(query) to return the following object:
 */
 
 function convertQueryToMap(query){ 
+
+  let queryArray = query.split('&');
+  let queryObject = {};
+
+  if(!query){
+    return queryObject;
+  }
+
+  queryArray.forEach(function(value, index){
+    queryArray[index] = value.split('=');
+    // does it contain a '.'?
+    queryArray[index][0] = queryArray[index][0].split('.');
+    let context = queryObject;
+    for(let key=0; key < queryArray[index][0].length - 1; key++){
+      if(!context[queryArray[index][0][key]]){
+        context[queryArray[index][0][key]] = {};      
+      }
+      context = context[queryArray[index][0][key]];
+    }
+    
+    context[queryArray[index][0][queryArray[index][0].length - 1]] = decodeURIComponent(queryArray[index][1]);
+  
+  });
+  
+  
+  
+  return queryObject;
   
 }
+
 
 
