5f6d19e83e50a947d75e3d5ced6618db3e38e4ba
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-06 13:42:51 -0800

    Added new problem

---
+++ stripURL.js
@@ -0,0 +1,13 @@
+/*
+Complete the method so that it does the following:
+
+Removes any duplicate query string parameters from the url
+Removes any query string parameters specified within the 2nd argument (optional array)
+stripUrlParams('www.codewars.com?a=1&b=2&a=2') // returns 'www.codewars.com?a=1&b=2'
+stripUrlParams('www.codewars.com?a=1&b=2&a=2', ['b']) // returns 'www.codewars.com?a=1'
+stripUrlParams('www.codewars.com', ['b']) // returns 'www.codewars.com'
+*/
+
+function stripUrlParams(url, paramsToStrip){
+
+}
