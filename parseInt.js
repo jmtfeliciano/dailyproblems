c020b8c540a6beb9262b809d2ad1d71de4315605
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-06 17:10:56 -0800

    Added new problem

---
+++ parseInt.js
@@ -0,0 +1,14 @@
+/* we want to convert a string into an integer. The strings simply represent the numbers in words.
+"two hundred forty-six" => 246
+"seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+
+Extra guidelines:  
+- The minimum number is "zero" (inclusively)
+- The maximum number, which must be supported is 1 million (inclusively)
+- The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+- Assume all tested numbers are valid, you don't need to validate them
+*/
+
+function parseInt(string) {
+
+}
