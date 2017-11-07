9dc532f44bf882890795114c54619ecaa401a775
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-07 11:56:35 -0800

    Implemented/completed functionality of function

--- highLow.js
+++ highLow.js
@@ -7,4 +7,15 @@ highAndLow("1 9 3 4 -5"); // return "9 -5"
 */
 
 function highAndLow(numbers){
+
+  let numbersArray = numbers.split(" ");
+  
+  numbersArray.forEach(function(value,index){
+    numbersArray[index] = Number(numbersArray[index]);
+  });
+  
+  numbersArray.sort(function(a, b){return b-a});
+  
+  return `${numbersArray[0]} ${numbersArray[numbersArray.length-1]}`;
+  
 }
