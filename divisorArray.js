b91a87b4f6629987625654f20f64951d3ae27e28
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-07 12:03:22 -0800

    Created problem with prompt and examples

---
+++ divisorArray.js
@@ -0,0 +1,13 @@
+/*
+Create a function named divisors/Divisors that takes an integer and 
+returns an array with all of the integer's divisors(except for 1 and the number itself). 
+If the number is prime return the string '(integer) is prime'
+e.g.
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+*/
+
+
+function divisors(integer) {
+}
