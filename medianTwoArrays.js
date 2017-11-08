94ecf42160f34ff5e519e19127e61af546a938b1
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-08 10:04:03 -0800

    Created new problem with JS prompt to do

---
+++ medianTwoArrays.js
@@ -0,0 +1,8 @@
+/*There are two sorted arrays nums1 and nums2 of size m and n respectively.
+Find the median of the two SORTED arrays. 
+If possible, make the overall run time complexity to be O(log (m+n)).
+*/
+
+var findMedianSortedArrays = function(nums1, nums2) {
+
+};
