a85d6ef31537d33c62759ef5175fd456c7a2817e
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-09 18:52:10 -0800

    Added new problem and prompt

---
+++ maximumBinaryTree.js
@@ -0,0 +1,18 @@
+/*
+Leetcode Medium problem prompt:
+
+Given an integer array with no duplicates. A maximum tree building on this array is defined as follow:
+
+The root is the maximum number in the array.
+The left subtree is the maximum tree constructed from left part subarray divided by the maximum number.
+The right subtree is the maximum tree constructed from right part subarray divided by the maximum number.
+Construct the maximum tree by the given array and output the root node of this tree.
+Def of tree:  function TreeNode(val) {
+                this.val = val;
+                this.left = this.right = null;
+              }
+*/
+
+var constructMaximumBinaryTree = function(nums) {
+
+};
