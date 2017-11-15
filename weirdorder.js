89d644b80e69fdb93704bf6bc3ff68885d6a6291
Josemari Feliciano <jfeliciano@alumni.harvard.edu>
2017-11-15 13:00:46 -0800

    Implemented weirdorder using reduce, forEach

--- weirdorder.js
+++ weirdorder.js
@@ -14,7 +14,35 @@ Test.assertEquals(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
 Test.assertEquals(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
 */
 
-function order(){
+function order(words){
+  if(!words){
+    return '';
+  }
+
+ words = words.split(' ');
+
+ let wordArray = [];
+
+ let findNumber = function(val){
+    for(let i=0; i < val.length; i++){
+      if(Number(val[i])){
+        return Number(val[i]);
+      }
+    }
+  };  
+  
+  words.forEach(function(val,i){
+    wordArray.push([findNumber(val), val]);      
+  });
+  
+  wordArray.sort(function(a,b){
+    return a[0] - b[0]; 
+  });
+
+  
+  let stringified = wordArray.reduce((acc,val) => {return acc+' '+val[1];}, '');
+
+  return stringified.slice(1,stringified.length);
+  
 
 }
-
