43e0b49705c5e5354e0bdc0bfd5f650001dc794b
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-22 19:13:18 -0800

    Implemented underscore with .call(arguments) and undefined as filter

--- underscoreDefaults.js
+++ underscoreDefaults.js
@@ -1,9 +1,22 @@
 /*
-Rewrite extend from underscore library
+Rewrite defaults from underscore library
 Should not copy if keys already exist
 */
 
-function underscoreDefaults(){
+function underscoreDefaults(obj){
 
+  let objects = Array.prototype.slice.call(arguments,1);
+  let combinedObject = obj;
+
+  //let falseys = [false, 0, -0, "", '', null, undefined, NaN,];
+
+  for(let i=0; i < objects.length; i++){
+    for(let key in objects[i]){
+      if(combinedObject[key] === undefined){
+        combinedObject[key] = objects[i][key]; 
+      }
+    }
+  }
   
+  return combinedObject;
 }
