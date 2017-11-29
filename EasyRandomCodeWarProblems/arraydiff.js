35357946a444dc1b53efc87adf92b9201e6d9c72
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-29 14:39:05 -0800

    Implemented functionality of array_diff

--- EasyRandomCodeWarProblems/arraydiff.js
+++ EasyRandomCodeWarProblems/arraydiff.js
@@ -12,5 +12,23 @@ If a value is present in b, all of its occurrences must be removed from the othe
 */
 
 function array_diff(a, b) {
+  let c = [];
+
+  for(let i=0; i < a.length; i++) {
+    let isPresent = false;
+    
+    for(let j=0; j < b.length; j++){
+      if(a[i]===b[j]){
+        isPresent = true;
+        j = b.length;
+      }
+    }
+
+    if(isPresent === false){
+      c.push(a[i]);
+    }
+  }
+
+  return c;
 
 }
